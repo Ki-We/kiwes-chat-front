1a4a7587731dbe062ca103a2155c53017643379a
kyuriiii <kyuri99@naver.com>
2023-07-08 12:21:27 +0900

    feat: Change db

--- socket-back2/src/model2/index.js
+++ socket-back2/src/model2/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PASSWORD}@${process.env.DB_MONGO_CLUSTER}.0q2wiuh.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DB_MONGO_USER}:${process.env.DB_MONGO_PASSWORD}@${process.env.DB_MONGO_CLUSTER}.9bac010.mongodb.net/?retryWrites=true&w=majority`,
     { dbName: "kiwes" }
   )
   .then(() => console.log("MongoDB connected"))
