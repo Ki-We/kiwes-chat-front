23463ea2b8e823f3598a48de417b8071df2f2ecd
kyuriiii <kyuri99@naver.com>
2023-07-17 19:57:27 +0900

    Feat: Delete unused code

--- socket-back2/src/route.js
+++ socket-back2/src/route.js
@@ -1,4 +1,3 @@
-const { Room, User } = require("./model");
 const { Chat, Log } = require("./model2");
 const { catch_error, createToken } = require("./utils");
 
@@ -37,24 +36,6 @@ router.post("/list", async (req, res) => {
     if (room != null) log["is_new"] = room.updatedAt <= log.createdAt;
   }
   res.send({ logs });
-});
-
-// 이전 삭제 필요 로직
-
-router.post("/login", async (req, res) => {
-  const user = await User.findOne({ where: { name: req.body.name } });
-  if (user == null) {
-    catch_error(null, res, "로그인 실패");
-    return false;
-  }
-
-  const token = await createToken(user);
-  res.send({ token });
-});
-
-router.get("/rooms", async (req, res) => {
-  const rooms = await Room.findAll().catch((err) => console.error(err));
-  res.send({ rooms });
 });
 
 module.exports = router;
